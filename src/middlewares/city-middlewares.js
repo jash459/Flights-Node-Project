3c8d7cbfcde42fb430b3821629f3f88f5ee795ce
Jash <jashhinger62@gamil.com>
2023-05-10 09:54:12 +0530

    Added middleware for create cities api

--- src/middlewares/city-middlewares.js
+++ src/middlewares/city-middlewares.js
@@ -4,10 +4,10 @@ const {ErrorResponse} = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 
 function validateCreateRequest(req, res, next){
-    if(!req.body.modelNumber){
-        ErrorResponse.message='something went wrong while creating a airplne';
+    if(!req.body.name){
+        ErrorResponse.message='something went wrong while creating a city';
         
-        ErrorResponse.error=new AppError(['Model Number not found in the incoming request in the correct form'],StatusCodes.BAD_REQUEST)
+        ErrorResponse.error=new AppError(['City name not found in the incoming request in the correct form'],StatusCodes.BAD_REQUEST)
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
     next();
--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const { CityController } = require('../../controllers')
+const { CityMiddlewares } = require('../../middlewares')
 
 const router = express.Router()
 // console.log('inside airplane routes')
 // api/v1/cities -->POST request
 router.post(
   '/',
+  CityMiddlewares.validateCreateRequest,
   CityController.createCity,
 )
 
