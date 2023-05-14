fd3d61de3a5cf50e4e43d03fd5ae9e343227b582
Jash <jashhinger62@gamil.com>
2023-05-14 08:23:51 +0530

    Added flights model with foreign key constraint defined during model defination ``` npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer ```

--- src/index.js
+++ src/index.js
@@ -11,34 +11,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', apiRoutes)
 
-app.listen(ServerConfig.PORT, async() => {
+app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`)
-  // bad code alert
-  const {City, Airport}= require('./models')
-  // const bengaluru = await City.findByPk(1, {include:{model:Airport}})
-  // console.log(bengaluru)
-  // const airport = await Airport.create({name : 'Kempegowda Airport', code:'BLR', cityId:1})
-  // const dbairport= await bengaluru.createAirport({name : 'Huballi Airport', code:'BL'})
-  // console.log(dbairport)
-  // const airportsInUdp=await bengaluru.getAirports()
-  // console.log(airportsInUdp)
-  // const hbairport = await Airport.findByPk(3)
-  // console.log(hbairport)
-  // await bengaluru.removeAirport(hbairport)
-  // await City.destroy({
-  //   where:{
-  //     id:1
-  //   }
-  // })
-
-  // const city = await City.findByPk(1)
-  // await city.createAirport({name: 'Indira Gandhi Airport', code:'IN'})
-
-  await City.destroy({
-      where:{
-        id:1
-      }
-    })
-
-
 })
